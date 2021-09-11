dcdc37c86b85ecca53bde9b28cd6f333732b9f34
Heron Hu <spectre.hhr@outlook.com>
2021-09-11 23:40:30 +0800

    [c Flipper] add class

---
+++ src/logic/Flipper.cpp
@@ -0,0 +1,1 @@
+#include "Flipper.h"
---
+++ src/logic/Flipper.h
@@ -0,0 +1,8 @@
+#ifndef BIT_FLIP_SRC_LOGIC_FLIPPER_H
+#define BIT_FLIP_SRC_LOGIC_FLIPPER_H
+
+namespace bit_flip::logic {
+class Flipper {};
+} // namespace bit_flip::logic
+
+#endif // BIT_FLIP_SRC_LOGIC_FLIPPER_H
