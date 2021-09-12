5401672082dbd13109d5821a3bfa7bdbf556abf6
Heron Hu <spectre.hhr@outlook.com>
2021-09-12 10:38:01 +0800

    [c Flipper] add ctor

--- src/logic/Flipper.cpp
+++ src/logic/Flipper.cpp
@@ -1,1 +1,15 @@
 #include "Flipper.h"
+#include <stdexcept>
+#include <utility>
+
+namespace fs = std::filesystem;
+
+namespace bit_flip::logic {
+Flipper::Flipper(const path_type &inPath, const path_type &outPath,
+                 std::size_t bufferSize)
+    : _inPath(fs::absolute(inPath)), _outPath(fs::absolute(outPath)),
+      _bufferSize(bufferSize != 0 ? bufferSize : _defaultBufferSize) {
+  if (_inPath == _outPath)
+    throw std::invalid_argument("input and output path can't be same");
+}
+} // namespace bit_flip::logic
--- src/logic/Flipper.h
+++ src/logic/Flipper.h
@@ -1,8 +1,23 @@
 #ifndef BIT_FLIP_SRC_LOGIC_FLIPPER_H
 #define BIT_FLIP_SRC_LOGIC_FLIPPER_H
 
+#include <filesystem>
+
 namespace bit_flip::logic {
-class Flipper {};
+class Flipper {
+public:
+  using path_type = std::filesystem::path;
+
+  explicit Flipper(const path_type &inPath, const path_type &outPath,
+                   std::size_t bufferSize);
+
+private:
+  const path_type _inPath;
+  const path_type _outPath;
+  const std::size_t _bufferSize;
+
+  static constexpr std::size_t _defaultBufferSize = 1024;
+};
 } // namespace bit_flip::logic
 
 #endif // BIT_FLIP_SRC_LOGIC_FLIPPER_H
